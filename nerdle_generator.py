4884dc10753305aaf858f72ad550b2b448f658b9
Michael Liu <5413532+michaelangeliu@users.noreply.github.com>
2022-02-10 22:02:24 -0600

    Wrap nerdle_generator with a guard function
    - Should prevent the script from being invoked unintentionally

--- nerdle_generator.py
+++ nerdle_generator.py
@@ -3,8 +3,6 @@ number_set = ['1','2','3','4','5','6','7','8','9']
 equals_set = ['=']
 operator_set = ['+','-','*','/']
 
-f = open("nerdle.txt", "a")
-
 def last_character_is_equals(curr_string):
     return curr_string[-1] == '='
 
@@ -52,23 +50,26 @@ def select_char_set(curr_string):
     else:
         return []
 
-for c1 in select_char_set(f""):
-    for c2 in select_char_set(f"{c1}"):
-        for c3 in select_char_set(f"{c1}{c2}"):
-            for c4 in select_char_set(f"{c1}{c2}{c3}"):
-                for c5 in select_char_set(f"{c1}{c2}{c3}{c4}"):
-                    for c6 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}"):
-                        for c7 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}{c6}"):
-                            for c8 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}{c6}{c7}"):
-                                eq = f"{c1}{c2}{c3}{c4}{c5}{c6}{c7}{c8}"
-                                if eq.count('=') != 1:
-                                    continue
+if __name__ == "__main__":
+    f = open("nerdle.txt", "a")
 
-                                eq_split = eq.split('=', 1)
-                                eq_str = eq_split[0]
-                                sum_str = eq_split[1]
+    for c1 in select_char_set(f""):
+        for c2 in select_char_set(f"{c1}"):
+            for c3 in select_char_set(f"{c1}{c2}"):
+                for c4 in select_char_set(f"{c1}{c2}{c3}"):
+                    for c5 in select_char_set(f"{c1}{c2}{c3}{c4}"):
+                        for c6 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}"):
+                            for c7 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}{c6}"):
+                                for c8 in select_char_set(f"{c1}{c2}{c3}{c4}{c5}{c6}{c7}"):
+                                    eq = f"{c1}{c2}{c3}{c4}{c5}{c6}{c7}{c8}"
+                                    if eq.count('=') != 1:
+                                        continue
 
-                                if (eval(eq_str) == int(sum_str)):
-                                    f.write(eq + '\n')
-                                    print(eq)
-f.close()
+                                    eq_split = eq.split('=', 1)
+                                    eq_str = eq_split[0]
+                                    sum_str = eq_split[1]
+
+                                    if (eval(eq_str) == int(sum_str)):
+                                        f.write(eq + '\n')
+                                        print(eq)
+    f.close()
